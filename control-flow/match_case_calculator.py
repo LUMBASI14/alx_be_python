57bd99a8fa477f66b427b97055f3e866831ba8c7
Lumbasi <154761897+LUMBASI14@users.noreply.github.com>
2025-05-31 15:38:04 +0300

    Update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,1 +1,18 @@
+first_number = int(input("Enter the first number:"))
+second_number = int(input("Enter the second number:"))
+operation = str(input("Choose the operation (+, -, *, /):"))
+                       
+match operation:
+  case "+":
+    sum = first_number + second_number
+    print(str(sum))
+  case "-":
+    subtraction = first_number - second_number
+    print(str(subtraction))
+  case "*":
+    multiplication = first_number * second_number
+    print(str(multiplication))
+  case "/":
+    division = first_number / second_number
+    print(str(division))
 
