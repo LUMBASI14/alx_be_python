150fc537b19f293ae0c1700cdb20361c05bcd407
Lumbasi <154761897+LUMBASI14@users.noreply.github.com>
2025-06-06 13:40:30 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,1 +1,30 @@
 
+def perform_operation(num1, num2, operation):
+  def addition(num1, num2):
+    return num1 + num2
+
+  def subtraction(num1, num2):
+    return num1 - num2
+
+  def multiplication(num1, num2):
+    return num1 * num2
+
+  def division(num1, num2):
+    if (num2 == 0):
+      return "math error"
+    else:
+      return num1 / num2
+
+  def calculation(num1, num2, operation):
+    match operation:
+      case "add":
+        return addition(num1, num2)
+      case "subtract":
+        return subtraction(num1, num2)
+      case "multiply":
+        return multiplication(num1, num2)
+      case "divide":
+        return division(num1, num2)
+      case _:
+        return "Invalid operations"
+        
