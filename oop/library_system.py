1679f1896150e73e4c4164becacefe65c26b0724
Lumbasi <154761897+LUMBASI14@users.noreply.github.com>
2025-06-16 19:01:03 +0300

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -12,6 +12,10 @@ class PrintBook(Book):
   def __init__(self, title, author, page_count):
     super().__init__(title, author)
     self.page_count = page_count
+    
+  def __str__(self, title, author, page_count):
+    return f""{self.title}" , "{self.author}" , "{self.page_count}""
+  
 
 class Library(books):
   def __init__(self):
