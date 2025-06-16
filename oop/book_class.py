3f6bdb11bc2a5463586dd3d70a62f234608fc650
Lumbasi <154761897+LUMBASI14@users.noreply.github.com>
2025-06-16 12:18:41 +0300

    Update book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,16 +1,17 @@
-def __init__(title, author, year):
-  self.title = title
-  self.author = author
-  self.year = year
-
-def __str__(self):
-  return f"{self.title} by {self.author}, published in {self.year}"
-
-def __repr__(self):
-  return f"Book( title={repr(self.title)}, author={repr(self.author)}, year={self.year})"
-
-def __del__(self):
-  return f"deleting {self.title}" 
+class Book:
+  def __init__(title, author, year):
+    self.title = title
+    self.author = author
+    self.year = year
   
+  def __str__(self):
+    return f"{self.title} by {self.author}, published in {self.year}"
   
+  def __repr__(self):
+    return f"Book( title={repr(self.title)}, author={repr(self.author)}, year={self.year})"
   
+  def __del__(self):
+    return f"deleting {self.title}" 
+    
+    
+    
