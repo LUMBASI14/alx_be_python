d79bd2acdab5edc0cd2362b75f531b238cfbde22
Lumbasi <154761897+LUMBASI14@users.noreply.github.com>
2025-06-16 13:00:29 +0300

    Create library_system.py

---
+++ oop/library_system.py
@@ -0,0 +1,26 @@
+class Book:
+  def __init__(self, title, author):
+    self.title = title
+    self.author = author
+    
+class Ebook(Book):
+  def __init__ (self, title, author, file_size):
+    super().__init__(title, author)
+    self.file_size = file_size
+    
+
+class PrintBook(Book):
+  def __init__(self, title, author, page_count):
+    super().__init__(title, author)
+    self.page_count = page_count
+
+class Library(books):
+  def __init__(self):
+    self.books = []
+    
+  def add_books(self, books):
+    self.books.append(books)
+
+  def list_books(self):
+    for book in self.books:
+      print (book)
