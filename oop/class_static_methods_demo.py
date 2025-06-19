b1adcf27f20aaf3f51d83a6cc3c6247ee9c029f9
Lumbasi <154761897+LUMBASI14@users.noreply.github.com>
2025-06-19 09:58:54 +0300

    Update class_static_methods_demo.py

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -1,13 +1,14 @@
 class Calculator: 
   calculation_type = "Arithmetic Operations"
   @staticmethod
+  # a static method
   def add(a, b):
-    return a+b
+    return a + b
 
   @classmethod
   def multiply(cls, a, b):
    # return calculation_type
-    return a*b
+    return a * b
   
 
   
