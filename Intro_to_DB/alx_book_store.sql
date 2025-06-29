f6b51e1237087e6d1bdf4b23edc504f87a479565
Lumbasi <154761897+LUMBASI14@users.noreply.github.com>
2025-06-29 22:02:40 +0300

    Update alx_book_store.sql

--- Intro_to_DB/alx_book_store.sql
+++ Intro_to_DB/alx_book_store.sql
@@ -12,40 +12,40 @@ alx_book_store = mysql.connector.connect(
 mycursor = alx_book_store.cursor()
 mycursor.execute(""" 
   CREATE TABLE Books(
-    book_id INT 
-    title VARCHAR(130)
-    author_id INT (Foreign Key)
-    price DOUBLE
-    publication_date DATE
-    PRIMARY KEY (book_id)
+    book_id INT, 
+    title VARCHAR(130),
+    author_id INT (Foreign Key),
+    price DOUBLE,
+    publication_date DATE,
+    PRIMARY KEY (book_id),
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
   )
   CREATE TABLE Authors(
-    author_id INT 
-    author_name VARCHAR(215)
+    author_id INT ,
+    author_name VARCHAR(215),
     PRIMARY KEY (author_id)
   )
   CREATE TABLE Customers(
-    customer_id INT 
-    customer_name VARCHAR(215)
-    email VARCHAR(215)
-    address TEXT
+    customer_id INT ,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT,
     PRIMARY KEY (customer_id)
   )
   CREATE TABLE Orders(
-    order_id INT 
-    customer_id INT 
-    order_date DATE
-    PRIMARY KEY (order_id)
+    order_id INT ,
+    customer_id INT ,
+    order_date DATE,
+    PRIMARY KEY (order_id),
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
   )
   CREATE TABLE Order_details(
-    orderdetailid INT  
-    order_id INT 
-    book_id INT 
-    quantity DOUBLE
-    PRIMARY KEY (orderdetailid)
-    FOREUGN KEY (order_id) REFERENCES Orders (order_id)
+    orderdetailid INT  ,
+    order_id INT ,
+    book_id INT ,
+    quantity DOUBLE,
+    PRIMARY KEY (orderdetailid),
+    FOREIGN KEY (order_id) REFERENCES Orders (order_id),
     FOREIGN KEY (book_id) REFERNCES Books (book_id)
   )
 """)
